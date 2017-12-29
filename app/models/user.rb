4d60ff2415a8d8373037ab9f28e9d973d3e5a584
Ramses Lopez <ramses.lopezs@gmail.com>
2017-12-29 19:33:31 -0400

    Details updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   acts_as_authentic { |c| c.crypto_provider = Authlogic::CryptoProviders::Sha512 }
+  after_create :update_photo
+
+  private
+
+  # Sets a placeholder photo
+  def update_photo
+    update(photo_url: Faker::Avatar.image(name.parameterize) )
+  end
+
 end
